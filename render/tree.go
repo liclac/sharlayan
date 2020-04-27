b63b87bfb4d448770d179deb4bfb8e8946a8abce
embr <git@liclac.eu>
2020-04-27 21:14:42 +0100

    cleanup

--- render/tree.go
+++ render/tree.go
@@ -34,17 +34,17 @@ func (d Dir) Node() Node {
 	return Node{Filename: d.Filename, Template: d.Template, Item: d.Links, Children: d.Children}
 }
 
-// Builds a root node, and a tree based on your Calibre metadata.
+// Builds a root node and a tree based on your Calibre metadata.
 func Root(cfg Config, meta *calibre.Metadata) Node {
 	var children []Node
 	var links []Link
 
 	if len(meta.Books) > 0 {
 		books := Dir{Filename: "book", Template: "_nav"}
-		for _, book := range meta.Books {
-			id := strconv.Itoa(book.ID)
-			books.Children = append(books.Children, Node{Filename: id, Template: "book", Item: book})
-			books.Links = append(books.Links, Link{Href: id, Text: book.Title})
+		for _, v := range meta.Books {
+			book := Node{Filename: strconv.Itoa(v.ID), Template: "book", Item: v}
+			books.Children = append(books.Children, book)
+			books.Links = append(books.Links, Link{Href: book.Filename, Text: v.Title})
 		}
 		children = append(children, books.Node())
 		links = append(links, Link{Href: books.Filename, Text: "Books"})
@@ -53,29 +53,31 @@ func Root(cfg Config, meta *calibre.Metadata) Node {
 	if !cfg.Author.NoIndex && len(meta.Authors) > 0 {
 		authors := Dir{Filename: "author", Template: "_nav"}
 		for _, v := range meta.Authors {
-			id := strconv.Itoa(v.ID)
-			authors.Children = append(authors.Children, Node{Filename: id, Template: "author", Item: v})
-			authors.Links = append(authors.Links, Link{Href: id, Text: v.Name})
+			author := Node{Filename: strconv.Itoa(v.ID), Template: "author", Item: v}
+			authors.Children = append(authors.Children, author)
+			authors.Links = append(authors.Links, Link{Href: author.Filename, Text: v.Name})
 		}
 		children = append(children, authors.Node())
 		links = append(links, Link{Href: authors.Filename, Text: "Authors"})
 	}
+
 	if !cfg.Series.NoIndex && len(meta.Series) > 0 {
 		series := Dir{Filename: "series", Template: "_nav"}
 		for _, v := range meta.Series {
-			id := strconv.Itoa(v.ID)
-			series.Children = append(series.Children, Node{Filename: id, Template: "series", Item: v})
-			series.Links = append(series.Links, Link{Href: id, Text: v.Name})
+			series_ := Node{Filename: strconv.Itoa(v.ID), Template: "series", Item: v}
+			series.Children = append(series.Children, series_)
+			series.Links = append(series.Links, Link{Href: series_.Filename, Text: v.Name})
 		}
 		children = append(children, series.Node())
 		links = append(links, Link{Href: series.Filename, Text: "Series"})
 	}
+
 	if !cfg.Tag.NoIndex && len(meta.Tags) > 0 {
 		tags := Dir{Filename: "tags", Template: "_nav"}
 		for _, v := range meta.Tags {
-			id := strconv.Itoa(v.ID)
-			tags.Children = append(tags.Children, Node{Filename: id, Template: "tag", Item: v})
-			tags.Links = append(tags.Links, Link{Href: id, Text: v.Name})
+			tag := Node{Filename: strconv.Itoa(v.ID), Template: "tag", Item: v}
+			tags.Children = append(tags.Children, tag)
+			tags.Links = append(tags.Links, Link{Href: tag.Filename, Text: v.Name})
 		}
 		children = append(children, tags.Node())
 		links = append(links, Link{Href: tags.Filename, Text: "Tags"})
