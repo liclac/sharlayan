bddfd519a2fa1e8f362faa9de2a163e66acc1cf6
embr <git@liclac.eu>
2020-05-13 15:31:27 +0100

    More Tree tests

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -9,28 +9,62 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestTree(t *testing.T) {
-	t.Run("File Root", func(t *testing.T) {
-		fs := memfs.New()
-		root := WrapNode("/", String("test.txt", "abc123"))
+// Build a tree consisting only of a single, non-directory node.
+func TestTreeSingleNode(t *testing.T) {
+	fs := memfs.New()
+	root := WrapNode("/", String("test.txt", "abc123"))
 
-		tree, err := New(fs, "/prefix", root.Node)
-		require.NoError(t, err)
-		assert.Equal(t, &Tree{
-			Filesystem: fs,
-			Path:       "/prefix",
-			Root:       root,
-			ByPath: map[string]*NodeWrapper{
-				"/test.txt": root,
-			},
-			ByLinkID: map[string]*NodeWrapper{},
-		}, tree)
+	tree, err := New(fs, "/prefix", root.Node)
+	require.NoError(t, err)
+	assert.Equal(t, &Tree{
+		Filesystem: fs,
+		Path:       "/prefix",
+		Root:       root,
+		ByPath: map[string]*NodeWrapper{
+			"/test.txt": root,
+		},
+		ByLinkID: map[string]*NodeWrapper{},
+	}, tree)
 
-		require.NoError(t, tree.Render())
-		assert.Equal(t, fsDumpNode{Filename: "/", Mode: 0755 | os.ModeDir, Nodes: []fsDumpNode{
-			fsDumpNode{Filename: "prefix", Mode: 0755 | os.ModeDir, Nodes: []fsDumpNode{
-				fsDumpNode{Filename: "test.txt", Mode: 0644, Data: []byte("abc123")},
+	require.NoError(t, tree.Render())
+	assert.Equal(t, fsDumpNode{Filename: "/", Mode: 0755 | os.ModeDir, Nodes: []fsDumpNode{
+		fsDumpNode{Filename: "prefix", Mode: 0755 | os.ModeDir, Nodes: []fsDumpNode{
+			fsDumpNode{Filename: "test.txt", Mode: 0644, Data: []byte("abc123")},
+		}},
+	}}, fsDumpT(t, fs, "/"))
+}
+
+// Build a tree that requires correctly recursing.
+func TestTreeDirs(t *testing.T) {
+	fs := memfs.New()
+	root := WrapNode("/",
+		Dir("",
+			String("test1.txt", "abc123"),
+			Dir("sub",
+				String("test2.txt", "def456"))))
+
+	tree, err := New(fs, "/prefix", root.Node)
+	require.NoError(t, err)
+	assert.Equal(t, &Tree{
+		Filesystem: fs,
+		Path:       "/prefix",
+		Root:       root,
+		ByPath: map[string]*NodeWrapper{
+			"/":              root,
+			"/test1.txt":     root.Children[0],
+			"/sub":           root.Children[1],
+			"/sub/test2.txt": root.Children[1].Children[0],
+		},
+		ByLinkID: map[string]*NodeWrapper{},
+	}, tree)
+
+	require.NoError(t, tree.Render())
+	assert.Equal(t, fsDumpNode{Filename: "/", Mode: 0755 | os.ModeDir, Nodes: []fsDumpNode{
+		fsDumpNode{Filename: "prefix", Mode: 0755 | os.ModeDir, Nodes: []fsDumpNode{
+			fsDumpNode{Filename: "sub", Mode: 0755 | os.ModeDir, Nodes: []fsDumpNode{
+				fsDumpNode{Filename: "test2.txt", Mode: 0644, Data: []byte("def456")},
 			}},
-		}}, fsDumpT(t, fs, "/"))
-	})
+			fsDumpNode{Filename: "test1.txt", Mode: 0644, Data: []byte("abc123")},
+		}},
+	}}, fsDumpT(t, fs, "/"))
 }
